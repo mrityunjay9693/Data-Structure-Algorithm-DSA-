5bbb584c4bfc04f5f2996143b2875488339a01ab
mrityunjay9693 <mkaykumar123@gmail.com>
2023-07-02 19:40:14 +0530

    reverse o given array

---
+++ ARRAY/C_in_Depth/SolvedEx/P85.c
@@ -0,0 +1,33 @@
+/* Write a C program to reverse the elements of an array */
+#include <stdio.h>
+int main()
+{
+    int arr[10] = {10, 6, 5, 5, 88, 100, 97, 31, 0, 7};
+    int temp;
+
+    for (int i=0,j=9; i<j; i++,j-- ) {
+        
+        //swapping is done here
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    printf("Given array : ");
+    printf("[10, 6, 5, 5, 88, 100, 97, 31, 0, 7]");
+    printf("\n");
+    printf("Reverse of given array : ");
+
+    //for loop for displaying after reverse of array
+    printf("[");
+    for (int i=0; i<=9; i++) {
+       
+        printf("%d",arr[i]);
+        printf(",");
+        
+    }
+    printf("]");
+    printf("\n");
+
+    return 0;
+}
