e2976fe70625e72ab0a1a0b6a85fa66f2efb5ab1
mrityunjay9693 <mkaykumar123@gmail.com>
2023-08-02 18:29:53 +0530

    sum of elemenst of array program

---
+++ ARRAY/10_ICSE/Q1.c
@@ -0,0 +1,31 @@
+/*
+ @mrityunjay9693
+ 
+ Q1: Write a program to input 10 numbers into an array and find sum of all numbers in it.
+ 
+ */
+
+#include <stdio.h>
+int main(){
+    int arr[10];
+    int sum = 0;
+
+    printf("Enter 10 elements in array arr: ");
+    printf("\n");
+
+    for (int i=0; i<10; i++) {
+        
+        scanf("%d",&arr[i]);
+        sum = sum + arr[i];   
+    }
+    
+    printf("%d",sum);
+    printf("\n");
+    
+    return 0;
+}
+
+
+
+
+
